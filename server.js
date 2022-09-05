84271d20bebeadfeab2aca756ded8b735e652dcd
vibraniumSwaleh <hus.swaleh@yahoo.com>
2022-09-05 22:55:05 +0100

    Code to print env variable added again

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
+const favoriteFood = process.env.FAVORITE_FOOD;
 
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function main() {
-  const favoriteFood = process.env.FAVORITE_FOOD;
-  while(true) {
+  while (true) {
     console.log("My favorite food is ${favoriteFood}");
     await sleep(5000);
   }
